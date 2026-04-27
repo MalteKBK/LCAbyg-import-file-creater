d6d1c6c89c7bce3d676b93db5e0058bc0805197d
MalteKBK <mkbk@arteliagroup.dk>
2026-04-27 13:28:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import json
 import streamlit as st
 
-from epd_to_lcabyg_helpers import (
+from epd_to_lcabyg_helpers_v7 import (
     build_import_zip_bytes,
     build_lcabyg_import_files,
     parse_epd_pdf_bytes,
